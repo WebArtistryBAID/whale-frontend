2d9480a491654c6ebab20611746f76e5d616c12f
Dreta <32196824+Dreta@users.noreply.github.com>
2024-09-18 09:07:22 +0800

    feat: add payment qr code

--- src/ui/pages/check/PageCheck.tsx
+++ src/ui/pages/check/PageCheck.tsx
@@ -27,6 +27,8 @@ import ComponentOrderedItem from '../order/ComponentOrderedItem.tsx'
 import { useState } from 'react'
 import { type PersistentStorage, usePersistentStorage } from '../../../data/persistentStorage.tsx'
 
+import payQR from './assets/pay-qr.jpg'
+
 export default function PageCheck(): JSX.Element {
     const { t } = useTranslation()
     const navigate = useNavigate()
@@ -92,31 +94,32 @@ export default function PageCheck(): JSX.Element {
                 </div>
 
                 <div id='main' className='h-full'>
-                    <div className='flex-grow p-8 pt-16'>
-                        <div className='flex flex-col items-center'>
-                            <h1 className='text-6xl font-bold font-display mb-3'>{order.data.number}</h1>
+                    <div className="flex-grow p-8 pt-16">
+                        <div className="flex flex-col items-center">
+                            <h1 className="text-6xl font-bold font-display mb-3">{order.data.number}</h1>
                             {(order.data.status === OrderStatus.notStarted || order.data.status === OrderStatus.inProgress)
                                 ? <>
-                                    <p className='text-sm text-center'>
-                                        <Trans i18nKey='check.estimateOrders' count={estimate.data.orders}
-                                               components={{ 1: <strong></strong> }}/>
+                                    <p className="text-sm text-center">
+                                        <Trans i18nKey="check.estimateOrders" count={estimate.data.orders}
+                                               components={{1: <strong></strong>}}/>
                                     </p>
-                                    <p className='text-sm mb-5 text-center'>
-                                        <Trans i18nKey='check.estimateTime' count={estimate.data.time}
-                                               components={{ 1: <strong></strong> }}/>
+                                    <p className="text-sm mb-5 text-center">
+                                        <Trans i18nKey="check.estimateTime" count={estimate.data.time}
+                                               components={{1: <strong></strong>}}/>
                                     </p></>
-                                : <p className='text-sm mb-5 text-center'>{t(`check.${order.data.status}_${order.data.type}`)}</p>}
+                                :
+                                <p className="text-sm mb-5 text-center">{t(`check.${order.data.status}_${order.data.type}`)}</p>}
                         </div>
 
-                        <div className='flex mb-5 justify-center'>
+                        <div className="flex mb-5 justify-center">
                             <div
                                 className={`flex flex-col items-center mr-3 ${order.data.status !== OrderStatus.notStarted ? 'text-gray-400' : 'text-accent-orange'}`}>
                                 <FontAwesomeIcon
                                     icon={order.data.status === OrderStatus.notStarted ? faHourglass : faHourglassR}
-                                    className='text-4xl mb-1' />
-                                <p className='text-xs text-center'>{t('check.status.notStarted_' + order.data.type)}</p>
+                                    className="text-4xl mb-1"/>
+                                <p className="text-xs text-center">{t('check.status.notStarted_' + order.data.type)}</p>
                                 {order.data.status === OrderStatus.notStarted
-                                    ? <p className='w-0 h-0 overflow-hidden'>{t('check.status.current')}</p>
+                                    ? <p className="w-0 h-0 overflow-hidden">{t('check.status.current')}</p>
                                     : null}
                             </div>
 
@@ -124,10 +127,10 @@ export default function PageCheck(): JSX.Element {
                                 className={`flex flex-col items-center mr-3 ${order.data.status !== OrderStatus.inProgress ? 'text-gray-400' : 'text-blue-500'}`}>
                                 <FontAwesomeIcon
                                     icon={order.data.status === OrderStatus.inProgress ? faHourglassHalf : faHourglassHalfR}
-                                    className='text-4xl mb-1' />
-                                <p className='text-xs text-center'>{t('check.status.inProgress_' + order.data.type)}</p>
+                                    className="text-4xl mb-1"/>
+                                <p className="text-xs text-center">{t('check.status.inProgress_' + order.data.type)}</p>
                                 {order.data.status === OrderStatus.inProgress
-                                    ? <p className='w-0 h-0 overflow-hidden'>{t('check.status.current')}</p>
+                                    ? <p className="w-0 h-0 overflow-hidden">{t('check.status.current')}</p>
                                     : null}
                             </div>
 
@@ -135,10 +138,10 @@ export default function PageCheck(): JSX.Element {
                                 className={`flex flex-col items-center mr-3 ${order.data.status !== OrderStatus.ready ? 'text-gray-400' : 'text-green-400'}`}>
                                 <FontAwesomeIcon
                                     icon={order.data.type === OrderType.delivery ? faTruck : (order.data.status === OrderStatus.ready ? faCircleCheck : faCircleCheckR)}
-                                    className='text-4xl mb-1' />
-                                <p className='text-xs text-center'>{t('check.status.ready_' + order.data.type)}</p>
+                                    className="text-4xl mb-1"/>
+                                <p className="text-xs text-center">{t('check.status.ready_' + order.data.type)}</p>
                                 {order.data.status === OrderStatus.ready
-                                    ? <p className='w-0 h-0 overflow-hidden'>{t('check.status.current')}</p>
+                                    ? <p className="w-0 h-0 overflow-hidden">{t('check.status.current')}</p>
                                     : null}
                             </div>
 
@@ -146,42 +149,45 @@ export default function PageCheck(): JSX.Element {
                                 className={`flex flex-col items-center ${order.data.status !== OrderStatus.pickedUp ? 'text-gray-400' : 'text-yellow-400'}`}>
                                 <FontAwesomeIcon
                                     icon={order.data.status === OrderStatus.pickedUp ? faFaceSmile : faFaceSmileR}
-                                    className='text-4xl mb-1' />
-                                <p className='text-xs text-center'>{t('check.status.pickedUp_' + order.data.type)}</p>
+                                    className="text-4xl mb-1"/>
+                                <p className="text-xs text-center">{t('check.status.pickedUp_' + order.data.type)}</p>
                                 {order.data.status === OrderStatus.pickedUp
-                                    ? <p className='w-0 h-0 overflow-hidden'>{t('check.status.current')}</p>
+                                    ? <p className="w-0 h-0 overflow-hidden">{t('check.status.current')}</p>
                                     : null}
                             </div>
                         </div>
 
-                        <div className='mb-3 w-full'>
+                        <div className="mb-3 w-full">
                             <ComponentIconText
-                                icon={<FontAwesomeIcon icon={faTriangleExclamation} className='text-yellow-400' />}>
+                                icon={<FontAwesomeIcon icon={faTriangleExclamation} className="text-yellow-400"/>}>
                                 {t('check.message.orderNumber')}
                             </ComponentIconText>
                         </div>
 
-                        <div className='mb-5 w-full'>
+                        <div className="mb-5 w-full">
                             <ComponentIconText
-                                icon={<FontAwesomeIcon icon={faTriangleExclamation} className='text-yellow-400' />}>
+                                icon={<FontAwesomeIcon icon={faTriangleExclamation} className="text-yellow-400"/>}>
                                 {t('check.message.pay')}
                             </ComponentIconText>
                         </div>
 
-                        <p className='text-gray-400 text-xs mb-2'>{t('check.totalPrice')}</p>
-                        <p className='font-display font-bold text-3xl mb-5'>¥{order.data.totalPrice}</p>
+                        <p className="text-gray-400 text-xs mb-2">{t('check.totalPrice')}</p>
+                        <p className="font-display font-bold text-3xl mb-5">¥{order.data.totalPrice}</p>
 
-                        <p className='text-gray-400 text-xs mb-2'>{t('check.orderType')}</p>
-                        <p className='font-display font-bold text-3xl mb-5'>{t(`order.type.${order.data.type}`)}</p>
+                        <p className="text-gray-400 text-xs mb-2">{t('check.orderType')}</p>
+                        <p className="font-display font-bold text-3xl mb-5">{t(`order.type.${order.data.type}`)}</p>
 
                         {order.data.type === OrderType.delivery
-                            ? <p className='text-gray-400 text-xs mb-2'>{t('check.deliveryRoom')}</p>
+                            ? <p className="text-gray-400 text-xs mb-2">{t('check.deliveryRoom')}</p>
                             : null}
                         {order.data.type === OrderType.delivery
-                            ? <p className='font-display font-bold text-3xl mb-5'>{order.data.deliveryRoom}</p>
+                            ? <p className="font-display font-bold text-3xl mb-5">{order.data.deliveryRoom}</p>
                             : null}
 
-                        <p className='text-gray-400 text-xs mb-2'>{t('check.products')}</p>
+                        <p className="text-gray-400 text-xs mb-2">{t('check.payQR')}</p>
+                        <img src={payQR} alt="QR code" className="w-full lg:w-1/2 mx-auto mb-5"/>
+
+                        <p className="text-gray-400 text-xs mb-2">{t('check.products')}</p>
                         {order.data.items.map((item: OrderedItemSchema) => <ComponentOrderedItem key={item.id}
                                                                                                  item={item}/>)}
 
